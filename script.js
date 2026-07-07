c3e1e7402957fc4d79b340deb2d639fc42da5b58
GuyIsGoatedAtGit <loganthatcher12@gmail.com>
2026-07-07 13:56:04 -0600

    commit2

---
+++ script.js
@@ -0,0 +1,14 @@
+// Array of image file paths
+const images = [
+  "images/togswt1.svg",
+  "images/togswt2.svg",
+  "images/togswt3.svg",
+];
+let currentIndex = 0;
+
+function flipImage() {
+  // Move to the next image index
+  currentIndex = (currentIndex + 1) % images.length;
+  // Update the image src
+  document.getElementById("togswtimg1").src = images[currentIndex];
+}
